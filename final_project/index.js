bf4f6de2a1c54798f1eac3b7725a9d0e5ccd945f
Srivalli Nalla <159496919+srivallinalla12@users.noreply.github.com>
2025-07-14 19:54:16 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,18 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if (req.session && req.session.authorization) {
+    let accessToken = req.session.authorization['accessToken'];
+
+    // If access token exists, user is authenticated
+    if (accessToken) {
+        next();
+    } else {
+        return res.status(403).json({ message: "User not authenticated" });
+    }
+} else {
+    return res.status(403).json({ message: "User not logged in" });
+}
 });
  
 const PORT =5000;
